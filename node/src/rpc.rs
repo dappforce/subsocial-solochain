db4946885c39d055a5e74a6cf93730486ee820f8
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-04-13 11:12:03 +0300

    Use primitives from primitives crate in node/rpc

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -7,7 +7,7 @@
 
 use std::sync::Arc;
 
-use subsocial_runtime::{opaque::Block, AccountId, Balance, Index, BlockNumber};
+use subsocial_primitives::{Block, AccountId, Balance, Index, BlockNumber};
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::{Error as BlockChainError, HeaderMetadata, HeaderBackend};
 use sp_block_builder::BlockBuilder;
