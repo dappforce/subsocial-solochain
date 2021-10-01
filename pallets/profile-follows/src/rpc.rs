44228918e498996ae9b51f4875d83f8604b55944
mk <nozdre@gmail.com>
2021-10-01 12:48:44 +0300

    profile follows frame v2 upgrade

--- pallets/profile-follows/src/rpc.rs
+++ pallets/profile-follows/src/rpc.rs
@@ -1,8 +1,8 @@
 use sp_std::prelude::*;
 
-use crate::{Module, Config};
+use crate::pallet::{Pallet, Config};
 
-impl<T: Config> Module<T> {
+impl<T: Config> Pallet<T> {
     pub fn filter_followed_accounts(account: T::AccountId, maybe_following: Vec<T::AccountId>) -> Vec<T::AccountId> {
         maybe_following.iter()
             .filter(|maybe_following| Self::account_followed_by_account((&account, maybe_following)))
