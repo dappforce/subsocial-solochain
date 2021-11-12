2df69585ca376ed75de070b95729a09675d6ac77
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-11-12 18:31:28 +0200

    Change `deposit` const fn

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -7,7 +7,7 @@ pub mod currency {
 	pub const MILLICENTS: Balance = CENTS / 1_000; // 1_000_000
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
-		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
+		items as Balance * 2_000 * CENTS + (bytes as Balance) * 100 * MILLICENTS
 	}
 }
 
