fc98c6223e4372374e57bbaad2a90da068916fd5
Shady Khalifa <shekohex@gmail.com>
2022-01-09 19:53:48 +0200

    Upgrade `pallet-spaces` to the new frame system.

--- pallets/spaces/src/rpc.rs
+++ pallets/spaces/src/rpc.rs
@@ -3,9 +3,13 @@ use codec::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 use sp_std::prelude::*;
 
-use pallet_utils::{bool_to_option, SpaceId, rpc::{FlatContent, FlatWhoAndWhen, ShouldSkip}};
+use pallet_utils::{
+    bool_to_option,
+    rpc::{FlatContent, FlatWhoAndWhen, ShouldSkip},
+    SpaceId,
+};
 
-use crate::{Module, Space, Config, FIRST_SPACE_ID};
+use crate::{Config, Pallet, Space, FIRST_SPACE_ID};
 
 #[derive(Eq, PartialEq, Encode, Decode, Default)]
 #[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
@@ -18,16 +22,28 @@ pub struct FlatSpace<AccountId, BlockNumber> {
 
     pub owner_id: AccountId,
 
-    #[cfg_attr(feature = "std", serde(skip_serializing_if = "ShouldSkip::should_skip"))]
+    #[cfg_attr(
+        feature = "std",
+        serde(skip_serializing_if = "ShouldSkip::should_skip")
+    )]
     pub parent_id: Option<SpaceId>,
 
-    #[cfg_attr(feature = "std", serde(skip_serializing_if = "ShouldSkip::should_skip", serialize_with = "bytes_to_string"))]
+    #[cfg_attr(
+        feature = "std",
+        serde(
+            skip_serializing_if = "ShouldSkip::should_skip",
+            serialize_with = "bytes_to_string"
+        )
+    )]
     pub handle: Option<Vec<u8>>,
 
     #[cfg_attr(feature = "std", serde(flatten))]
     pub content: FlatContent,
 
-    #[cfg_attr(feature = "std", serde(skip_serializing_if = "ShouldSkip::should_skip"))]
+    #[cfg_attr(
+        feature = "std",
+        serde(skip_serializing_if = "ShouldSkip::should_skip")
+    )]
     pub is_hidden: Option<bool>,
 
     pub posts_count: u32,
@@ -37,20 +53,30 @@ pub struct FlatSpace<AccountId, BlockNumber> {
 }
 
 #[cfg(feature = "std")]
-fn bytes_to_string<S>(field: &Option<Vec<u8>>, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
+fn bytes_to_string<S>(field: &Option<Vec<u8>>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
     let field_unwrapped = field.clone().unwrap_or_default();
     // If Bytes slice is invalid, then empty string will be returned
-    serializer.serialize_str(
-        std::str::from_utf8(&field_unwrapped).unwrap_or_default()
-    )
+    serializer.serialize_str(std::str::from_utf8(&field_unwrapped).unwrap_or_default())
 }
 
 impl<T: Config> From<Space<T>> for FlatSpace<T::AccountId, T::BlockNumber> {
     fn from(from: Space<T>) -> Self {
         let Space {
-            id, created, updated, owner,
-            parent_id, handle, content, hidden, posts_count,
-            hidden_posts_count, followers_count, ..
+            id,
+            created,
+            updated,
+            owner,
+            parent_id,
+            handle,
+            content,
+            hidden,
+            posts_count,
+            hidden_posts_count,
+            followers_count,
+            ..
         } = from;
 
         Self {
@@ -69,9 +95,12 @@ impl<T: Config> From<Space<T>> for FlatSpace<T::AccountId, T::BlockNumber> {
     }
 }
 
-impl<T: Config> Module<T> {
-    pub fn get_spaces_by_ids(space_ids: Vec<SpaceId>) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
-        space_ids.iter()
+impl<T: Config> Pallet<T> {
+    pub fn get_spaces_by_ids(
+        space_ids: Vec<SpaceId>,
+    ) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
+        space_ids
+            .iter()
             .filter_map(|id| Self::require_space(*id).ok())
             .map(|space| space.into())
             .collect()
@@ -101,11 +130,17 @@ impl<T: Config> Module<T> {
         Self::get_spaces_slice(start_id, limit, |_| true)
     }
 
-    pub fn get_public_spaces(start_id: u64, limit: u64) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
+    pub fn get_public_spaces(
+        start_id: u64,
+        limit: u64,
+    ) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
         Self::get_spaces_slice(start_id, limit, |space| space.is_public())
     }
 
-    pub fn get_unlisted_spaces(start_id: u64, limit: u64) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
+    pub fn get_unlisted_spaces(
+        start_id: u64,
+        limit: u64,
+    ) -> Vec<FlatSpace<T::AccountId, T::BlockNumber>> {
         Self::get_spaces_slice(start_id, limit, |space| space.is_unlisted())
     }
 
@@ -119,7 +154,10 @@ impl<T: Config> Module<T> {
             .map(|space| space.into())
     }
 
-    fn get_space_ids_by_owner<F: FnMut(&Space<T>) -> bool>(owner: T::AccountId, mut compare_fn: F) -> Vec<SpaceId> {
+    fn get_space_ids_by_owner<F: FnMut(&Space<T>) -> bool>(
+        owner: T::AccountId,
+        mut compare_fn: F,
+    ) -> Vec<SpaceId> {
         Self::space_ids_by_owner(owner)
             .iter()
             .filter_map(|space_id| Self::require_space(*space_id).ok())
