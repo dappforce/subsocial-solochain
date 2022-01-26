e3f3472631ca84e5e271405754004495aebf5957
TarekkMA <tarekkma@gmail.com>
2022-01-26 20:02:39 +0200

    Small fix for mocks.rs

--- pallets/free-calls/src/mock.rs
+++ pallets/free-calls/src/mock.rs
@@ -1,71 +1,80 @@
-use crate as pallet_free_calls;
-use frame_support::{parameter_types, sp_io};
-use frame_support::traits::Everything;
-use frame_system::EnsureRoot;
+use sp_core::H256;
+use sp_io::TestExternalities;
 use sp_runtime::{
     traits::{BlakeTwo256, IdentityLookup}, testing::Header, Storage
 };
-pub use subsocial_primitives::{AccountId, Signature, Balance, Index};
-use subsocial_primitives::BlockNumber;
+
+use crate as pallet_free_calls;
+
+use frame_support::{
+    parameter_types,
+    assert_ok,
+    dispatch::DispatchResultWithPostInfo,
+};
+use frame_support::traits::Everything;
+use frame_system as system;
+use frame_system::EnsureRoot;
+
+pub(crate) type AccountId = u64;
+pub(crate) type BlockNumber = u64;
+
 use crate::mock::time::*;
 use crate::WindowConfig;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
-
 pub mod time {
-    use subsocial_primitives::{Moment, BlockNumber};
+    use crate::mock::BlockNumber;
 
-    pub const MILLISECS_PER_BLOCK: Moment = 6000;
-    pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
+    pub const MILLISECS_PER_BLOCK: BlockNumber = 6000;
+    pub const SLOT_DURATION: BlockNumber = MILLISECS_PER_BLOCK;
 
     // These time units are defined in number of blocks.
-    pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+    pub const MINUTES: BlockNumber = 60_000 / MILLISECS_PER_BLOCK;
     pub const HOURS: BlockNumber = MINUTES * 60;
     pub const DAYS: BlockNumber = HOURS * 24;
 }
 
-// Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		FreeCalls: pallet_free_calls::{Pallet, Call, Storage, Event<T>},
-	}
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: system::{Pallet, Call, Config, Storage, Event<T>},
+        FreeCalls: pallet_free_calls::{Pallet, Call, Storage, Event<T>},
+    }
 );
 
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
+    pub const BlockHashCount: u64 = 250;
+    pub const SS58Prefix: u8 = 28;
 }
 
-impl frame_system::Config for Test {
-    type BaseCallFilter = frame_support::traits::Everything;
+impl system::Config for Test {
+    type BaseCallFilter = ();
     type BlockWeights = ();
     type BlockLength = ();
-    type DbWeight = ();
     type Origin = Origin;
     type Call = Call;
     type Index = u64;
     type BlockNumber = BlockNumber;
-    type Hash = Hash;
+    type Hash = H256;
     type Hashing = BlakeTwo256;
-    type AccountId = u64;
+    type AccountId = AccountId;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Header = Header;
     type Event = Event;
     type BlockHashCount = BlockHashCount;
+    type DbWeight = ();
     type Version = ();
     type PalletInfo = PalletInfo;
     type AccountData = ();
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
-    type SS58Prefix = SS58Prefix;
+    type SS58Prefix = ();
     type OnSetCode = ();
 }
 
@@ -85,6 +94,6 @@ impl pallet_free_calls::Config for Test {
 }
 
 // Build genesis storage according to the mock runtime.
-pub fn new_test_ext() -> sp_io::TestExternalities {
+pub fn new_test_ext() -> TestExternalities {
     frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
 }
