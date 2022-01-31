fe73bdb39d8da7030f4aee8e7bdae36e62e41d7b
TarekkMA <tarekkma@gmail.com>
2022-01-31 15:18:09 +0200

    refactor accounts to functions

--- pallets/locker-mirror/src/tests.rs
+++ pallets/locker-mirror/src/tests.rs
@@ -1,19 +1,25 @@
 #![allow(non_snake_case)]
 use frame_benchmarking::account;
-use crate::{mock::*, LockedInfoByAccount, BalanceOf, LockedInfo};
+use crate::{mock::*, LockedInfoByAccount, BalanceOf, LockedInfo, Config};
 use frame_support::{assert_ok, assert_noop, assert_err};
-use sp_io::KillStorageResult::AllRemoved;
 use sp_runtime::DispatchError::BadOrigin;
+
+
+fn caller_account<T: Config>() -> T::AccountId {
+    account("Caller", 0, 0)
+}
+
+fn subject_account<T: Config>() -> T::AccountId {
+    account("Subject", 1, 1)
+}
 
 #[test]
 fn set_locked_info__should_fail_when_not_manager_origin() {
     new_test_ext().execute_with(|| {
-        let caller = account("Test Account", 1 ,4);
-        let subject = account("Test Account", 1, 4);
         assert_err!(
             LockerMirror::set_locked_info(
-                Origin::signed(caller),
-                subject,
+                Origin::signed(caller_account::<Test>()),
+                subject_account::<Test>(),
                 LockedInfo::<<Test as frame_system::Config>::BlockNumber, BalanceOf<Test>> {
                     locked_amount: 1_000_000_000_000u64.into(),
                     unlocks_at: 11u32.into(),
@@ -28,12 +34,10 @@ fn set_locked_info__should_fail_when_not_manager_origin() {
 #[test]
 fn clear_locked_info__should_fail_when_not_manager_origin() {
     new_test_ext().execute_with(|| {
-        let caller = account("Test Account", 1 ,4);
-        let subject = account("Test Account2", 2, 5);
         assert_err!(
             LockerMirror::clear_locked_info(
-                Origin::signed(caller),
-                subject,
+                Origin::signed(caller_account::<Test>()),
+                subject_account::<Test>(),
             ),
             BadOrigin,
         );
